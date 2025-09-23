4c1b22999a78978b7ca81583100aec2b9152a918
hgp22 <henriquepereira31337@gmail.com>
2025-09-23 19:42:11 +0100

    feat: add testimonials as carousel

---
+++ src/components/Testimonial.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  testimonial: string
+  author: string
+  major: string
+}>()
+</script>
+
+<template>
+  <div>
+    <div class="justify-center px-12">
+      <div>
+        <h2 class="text-7xl font-semibold text-purple max-sm:text-3xl">&quot;</h2>
+        <p class="mt-4 mb-4 text-2xl max-sm:text-base leading-relaxed">{{ testimonial }}</p>
+        <h2 class="text-right text-7xl font-semibold text-purple max-sm:text-3xl">&quot;</h2>
+        <p class="mb-4 text-2xl leading-relaxed italic max-sm:text-xl">- {{ author }}, {{ major }}</p>
+      </div>
+    </div>
+  </div>
+</template>
