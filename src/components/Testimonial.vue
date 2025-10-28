e55c4efc15a0d1961d48c91fa8f7b9112cb41d67
hgp22 <henriquepereira31337@gmail.com>
2025-10-28 12:03:43 +0000

    chore: change stlying to theme standards

--- src/components/Testimonial.vue
+++ src/components/Testimonial.vue
@@ -10,9 +10,9 @@ defineProps<{
   <div>
     <div class="justify-center px-12">
       <div>
-        <h2 class="text-7xl font-semibold text-purple max-sm:text-3xl">&quot;</h2>
+        <h2 class="text-7xl font-semibold text-primary max-sm:text-3xl">&quot;</h2>
         <p class="mt-4 mb-4 text-2xl max-sm:text-base leading-relaxed">{{ testimonial }}</p>
-        <h2 class="text-right text-7xl font-semibold text-purple max-sm:text-3xl">&quot;</h2>
+        <h2 class="text-right text-7xl font-semibold text-primary max-sm:text-3xl">&quot;</h2>
         <p class="mb-4 text-2xl leading-relaxed italic max-sm:text-xl">- {{ author }}, {{ major }}</p>
       </div>
     </div>
